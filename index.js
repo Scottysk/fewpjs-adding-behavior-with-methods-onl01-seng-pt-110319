120583340d8d343b61a55175128229d92815092d
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-07-01 00:55:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 class Cat {
-  constructor(speak) {
-    this.speak = speak;
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
   }
-  
+  speak()
 }
 
 class Dog {
-  
+  constructor(name, sex) {
+  this.name = name;
+  this.sex = sex;
+  }
 }
 
 class Bird {
-  
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
 }
 
